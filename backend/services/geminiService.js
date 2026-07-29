9438834cc8a926fb2fa89563cb893e915e2d6177
aasthagarg06 <gargaastha161@gmail.com>
2026-07-29 13:58:33 +0530

    Gemini

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -2,7 +2,7 @@ import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("Has GEMINI_API_KEY:", !!process.env.GEMINI_API_KEY);
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
